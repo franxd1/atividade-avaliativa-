aeb4eb5bdcfa5abd4ec13868db132782e078e545
franxd1 <franciscoalvescabral22@gmail.com>
2023-09-21 20:36:31 -0300

    questao 4

---
+++ questao4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+  double salario;
+  char call;
+  double sfim;
+  
+  printf ("classe:");
+  scanf("%c",&call); 
+  
+  printf ("salario:");
+  scanf("%lf",&salario);
+   //printf ("%.0f",salario);
+  
+   switch (call){
+      case 'a':
+      
+       sfim =  (5 * salario) /100 + salario;
+       break;
+       
+      case 'b' :
+       sfim =  (7 * salario) /100 + salario;
+       break;
+       
+      case 'c':
+        sfim =  (8 * salario) /100 + salario;
+        break;
+   }
+     printf ("%0.2f",sfim);
+  
+ return 0;
+}
